d0a917d9a3b03d2d0ad2b1a8e4cc30e94f1cdda8
Joe Haines <hello@joehaines.co.uk>
2015-01-17 17:23:40 +0000

    Delete story deletes chapters; delete story method
    - Now deletes chapters when deleting a story
    - Deleting a story is now a method

--- TODO.md
+++ TODO.md
@@ -1,6 +1,8 @@
 #TODO
 
-- Remove chapters when removing a story
-- Notifications shouldn't notify if the user to be notified are on the same page that the notification is for (possible?)
+- Notifications shouldn't be created if the user to be notified is on the page that the notification is for (possible?)
 - Add mobile compatability for delete story/chapter buttons
+
+#Done
+- Remove chapters when removing a story
 - Make story_title call a method to delete story
--- client/templates/story_title.js
+++ client/templates/story_title.js
@@ -9,34 +9,36 @@ Template.storyTitle.events({
         e.preventDefault();
 
         var storyId = this._id;
-        var currentStoryTitle = Stories.findOne({_id: storyId}).title;
+        var story = Stories.findOne({_id: storyId});
+        var storyTitle = story.title;
 
         swal({
             title: 'Are you sure?',
-            text: 'Do you really want to delete "' + currentStoryTitle + '"?\nThis can\'t be undone!',
+            text: 'Do you really want to delete "' + storyTitle + '"?\nThis can\'t be undone!',
             type: 'warning',
             confirmButtonText: 'Confirm',
             showCancelButton: true,
             closeOnConfirm: false,
             confirmButtonColor: '#33C3F0'
         }, function() {
-            // remove returns 1 on success, 0 on false
-            var success = Stories.remove(storyId);
-            if(success == 1) {
-                swal({
-                    title: 'Deleted!',
-                    text: 'Successfully deleted "' + currentStoryTitle + '"!',
-                    type: 'success',
-                    confirmButtonColor: '#33C3F0'
-                });                
-            } else {
-                swal({
-                    title: 'Oops! :(',
-                    text: 'We couldn\t delete "' + currentStoryTitle + '" right now, try again later.',
-                    type: 'error',
-                    confirmButtonColor: '#33C3F0'
-                });                
-            }
+            Meteor.call('deleteStory', story, function(error) {
+                if(error) {
+                    return swal({
+                        title: 'Oops! :(',
+                    text: 'We couldn\t delete "' + storyTitle + '" right now, try again later.',
+                        type: 'error',
+                        confirmButtonColor: '#33C3F0'
+                    });
+                } else {
+                    swal({
+                        title: 'Deleted!',
+                        text: 'Successfully deleted "' + storyTitle + '"!',
+                        type: 'success',
+                        confirmButtonColor: '#33C3F0',
+                        timer: 2000
+                    });
+                }
+            });
         });
     }
 });
