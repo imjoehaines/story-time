eb6f91508860f53f452353ac7df2603564e23232
Joe Haines <hello@joehaines.co.uk>
2015-01-27 19:19:13 +0000

    More work on chapter turn taking

--- client/templates/story_page.js
+++ client/templates/story_page.js
@@ -1,6 +1,31 @@
 Template.storyPage.helpers({
     storyTitle: function() {
         return Stories.findOne({_id: this.story._id}).title;
+    },
+
+    // returns the other attributes for the chapter input box
+    // (disabled & placeholder)
+    otherAttributes: function() {
+        var isLastAuthor = this.story.lastChapterBy === Meteor.user()._id;
+
+        if(isLastAuthor) {
+            placeholderText = 'You can\'t write two chapters in a row, wait for someone else to write one!';
+            isDisabled = true;
+        } else {
+            placeholderText = 'Add a chapter to this story...';
+            // has to be undefined - false will still add disabled attribute to inputbox ???
+            isDisabled = undefined;
+        }
+
+        return {
+            disabled: isDisabled,
+            placeholder: placeholderText
+        };
+    },
+
+    // returns true if the user wrote the last chapter in the story
+    isLastAuthor: function() {
+        return this.story.lastChapterBy === Meteor.user()._id;
     }
 });
 
@@ -23,6 +48,13 @@ Template.storyPage.events({
                         title: 'Oops! :(',
                         text: 'We couldn\'t create that chapter right now, try again later.',
                         type: 'error',
+                        confirmButtonColor: '#33C3F0'
+                    });
+                } else if(result.success === false) {
+                    return swal({
+                        title: 'Oops! :(',
+                        text: 'You can\'t write two chapters in a row, wait for someone else to write one before trying again.',
+                        type: 'info',
                         confirmButtonColor: '#33C3F0'
                     });
                 }
